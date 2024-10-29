40b5b0c531edb0b95acff51e6ec94c601a9e1b11
Trần Văn Tùng <74578965+tranvantung2002@users.noreply.github.com>
2024-10-29 23:27:55 +0700

    cập nhật login theo mvc

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Controller.LoginController;
 import View.LoginFrame;
 import View.MainFrame;
 
@@ -6,29 +7,8 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            // Hiển thị màn hình login
             LoginFrame loginFrame = new LoginFrame();
-
-            // Sử dụng SwingWorker để kiểm tra trạng thái đăng nhập
-            new SwingWorker<Void, Void>() {
-                @Override
-                protected Void doInBackground() {
-                    while (!loginFrame.isLoggedIn()) {
-                        try {
-                            Thread.sleep(1000); // Ngủ 1 giây
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    return null;
-                }
-
-                @Override
-                protected void done() {
-                    // Nếu đăng nhập thành công, mở màn hình chính
-                    new MainFrame();
-                }
-            }.execute();
+            new LoginController(loginFrame); // Khởi tạo Controller với View
         });
     }
 }
